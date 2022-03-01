22fd9bf1b4872a9b35f2a52584ddadb10357fd07
松井晴也 <>
2022-03-01 23:43:45 +0900

    アイキャッチ横に筆者の名前を表示

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,4 +3,8 @@ class Board < ApplicationRecord
     validates :description, presence: :true
 
     belongs_to :user
+
+    def author_name
+        user.display_name
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+
+  def display_name
+    self.email.split('@').first
+  end
+
 end
