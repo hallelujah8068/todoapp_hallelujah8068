4e2a6e31ffeb1c48120fc55d9a804b5e407b9fff
松井晴也 <>
2022-03-06 22:59:53 +0900

    boardの詳細画面からtaskの一覧を表示できるよう変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,27 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user!
+
     def index
-        @tasks = Tasks.all
+        @board = Board.find(params[:board_id])
+        @tasks = @board.tasks
     end
 
     def show
+        board = Board.find(params[:board_id])
         @task = Task.find(params[:id])
     end
 
     def new
         @board = Board.find(params[:board_id])
-        @task = current_user.tasks.build
+        @task = @board.tasks.build
     end
 
     def create
-        @task = current_user.tasks.build(task_params)
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build(task_params)
+        @task.user_id = current_user.id
         if @task.save
-            redirect_to board_tasks_path(@board), notice: '保存できたよ'
+            redirect_to board_tasks_path(params[:board_id]), notice: '保存できたよ'
         else
             flash.now[:error] = '保存に失敗したよ'
             render :new
@@ -23,19 +29,30 @@ class TasksController < ApplicationController
     end
 
     def  edit
-        
+        @board = Board.find(params[:board_id])
+        @task = @board.tasks.find(params[:id])
     end
 
     def update
-        
+        board = Board.find(params[:board_id])
+        @task = board.tasks.find(params[:id])
+        if @task.update(task_params)
+          redirect_to board_tasks_path(params[:board_id]), notice: '更新できました'
+        else
+          flash.now[:error] = '更新できませんでした'
+          render :edit
+        end 
     end
 
     def destroy
-        
+        board = Board.find(params[:board_id])
+        task = board.tasks.find(params[:id])
+        task.destroy!
+        redirect_to board_tasks_path(params[:board_id]), notice: '削除に成功しました'
     end
 
     private
     def task_params
-        params.require(:task).permit(:title, :description, :eyecatch, :deadline)
+        params.require(:task).permit(:title, :description, :eyecatch, :deadline, :board_id)
     end
 end
